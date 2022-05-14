3cc69b44aefc0686fe07de87cb0adb8d9bfaef6d
Jeroen Ooms <jeroenooms@gmail.com>
2022-05-14 09:58:30 +0000

    update

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Debian dir
+rm *.deb
+wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.02.2-485-amd64.deb
+ar -xv *.deb control.tar.xz
+tar xvf control.tar.xz ./postinst ./postrm ./control
+cp -fv postinst postrm debian/
+rm -f xvf control.tar.xz postinst postrm
+echo "Done!"
+echo "Please update control and changelog file manually (source instead of binary)"
+echo "Input:"
+cat control
